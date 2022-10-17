76b9dce020f1663558a228d56b354c9ecbec9776
PianoPiston <114503355+PianoPiston@users.noreply.github.com>
2022-10-17 12:11:43 +0000

    Add files via upload

---
+++ opcalc-en.py
@@ -0,0 +1,91 @@
+from time import sleep
+
+def summ(x):
+    accum = 0
+    for valuu in x.values(): #this function is meant for future expansion, but only used once currently.
+        accum += int(valuu)
+    return(accum)
+
+banner = r"""
+ ________  ________        ________  ________  ___       ________         
+|\   __  \|\   __  \      |\   ____\|\   __  \|\  \     |\   ____\        
+\ \  \|\  \ \  \|\  \     \ \  \___|\ \  \|\  \ \  \    \ \  \___|        
+ \ \  \\\  \ \   ____\     \ \  \    \ \   __  \ \  \    \ \  \           
+  \ \  \\\  \ \  \___|      \ \  \____\ \  \ \  \ \  \____\ \  \____  ___ 
+   \ \_______\ \__\          \ \_______\ \__\ \__\ \_______\ \_______\\__\
+    \|_______|\|__|           \|_______|\|__|\|__|\|_______|\|_______\|__|
+"""
+
+print(banner)
+print("Welcome to opintopiste calculator!")
+print("_______________________________________________________________________\n")
+
+print("WARNING: please make sure that the data file is in the same folder as this program.")
+print(
+"""Please make sure that in VScode you select the right folder containing the datafile and program.\n
+To select a folder, navigate to the upper left icon, hover over the icons and find explorer,
+there you can choose the folder containing this program and the datafile.\n """)
+
+debugging = input('do you wish to turn on debugging mode? for normal use please select "n" and press enter. y/n: ')
+if debugging != "y":
+    if debugging != "n":
+        print("debugging input is wrong, quitting...")
+        sleep(3) #i want to emphasize the misinput so ill add a small delay
+        quit()
+
+
+thefile = input("please write the filename containing your subjects and points\nwith file extension(.txt) and press enter: ")
+
+with open(thefile, 'r') as wl:
+    data = wl.readlines()
+    dataf = []                  # this function opens up the file, splits the table and puts every 
+    for subject in data:        # object into a list.
+        subject = subject[:-2]
+        dataf.append(subject.split("\t"))
+
+    subjects = []
+
+    for sub in dataf:
+        third = sub[2]
+        subjects.append(third)
+        
+    if debugging == "y":
+        print("these are the subjects that will be calculated:")
+        print(subjects)
+
+    subjects2 = []
+    subnpoints = {}
+
+    print("Calculating..........\n")
+
+    # the line below will check to see if the subject has a special character, 
+    # and then add a key value pair of "subject: op".
+    # if the subject already exists in the dictionary as a special op amount,
+    # it will override and nothing will happen. - thus disregarded.
+    # however if it already exists in the dictionary as a normal subject
+    # with op set to 2, it will run line 42, - thus disregarded.
+    # if its completely new, it will set op to 2. - thus setting it as default value.
+
+    for sub in subjects:
+        if sub[-1] == ")":
+            num = sub[-5:]
+            sub = sub[:-5]
+            if debugging == "y":
+                print(sub,num)
+            point = num[1]
+            subnpoints[sub] = point
+        elif sub in subnpoints:
+            if debugging == "y":
+                print("subject {} already in subjects".format(sub))
+        else:
+            subnpoints[sub] = 2
+
+    print("below, are your subjects and OP seperated by a ':'.")
+    if debugging == "y":
+        print(subnpoints)
+
+    for subject, point in subnpoints.items():
+        print("{}: {} opintopisteitä".format(subject, point))
+
+    print("_______________________")
+    print("You have a total of: ",summ(subnpoints),'"opintopisteitä"')
---
+++ opcalcgui-v1.py
@@ -0,0 +1,133 @@
+import pygame
+from sys import exit
+from random import randint
+
+def summ(x):
+    accum = 0
+    for valuu in x.values(): #summmmmm
+        accum += int(valuu)
+    return(accum)
+
+def clicked(button): #checks to see if click happens inside location of button
+    if button[0] <= mouse[0] <= button[0]+button[2] and button[1] <= mouse[1] <= button[1]+button[3]:
+        return True
+    else:
+        return False
+
+pygame.init()
+active = True
+screen = pygame.display.set_mode((870,640))
+pygame.display.set_caption("Op calculator")
+clock = pygame.time.Clock()
+width = screen.get_width()
+height = screen.get_height()
+font1 = pygame.font.SysFont('Arial',35)
+font2 = pygame.font.SysFont('Arial',15)
+text = font1.render('calculate' , True , "black")
+
+#"boxes", <name>b means its a button
+calcb = [700,420,150,50] # location in x, location in y, size in x, size in y
+outputbackground = [10,10,850,500]
+opback = [10,520,850,75]
+
+#"global variables"
+gsubjects = []
+
+while True:
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    #checks if a mouse is clicked
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+            if clicked(calcb):
+                if active == False:
+                    break
+                active = False
+                with open("op.txt", 'r') as wl: #
+                    data = wl.readlines()
+                    dataf = []                  # this function opens up the file, splits the table and puts every 
+                    for subject in data:        # object into a list.
+                        subject = subject[:-2]
+                        dataf.append(subject.split("\t"))
+
+                    subjects = []
+
+                    for sub in dataf:
+                        if sub[0][3:8] == "jakso":
+                            sub = sub[3]
+                            print("found 'jakso' header in this subject: ",sub)
+                            subjects.append(sub)
+                        else:
+                            third = sub[2]
+                            subjects.append(third)
+                
+                    subjects2 = []
+                    subnpoints = {}
+
+                    for sub in subjects:
+                        if sub[-1] == ")":
+                            num = sub[-5:]
+                            sub = sub[:-5]
+                            point = num[1]
+                            subnpoints[sub] = point
+                        else:
+                            subnpoints[sub] = 2
+
+                    print("below, are your subjects and OP seperated by a ':'.")
+                    for subject, point in subnpoints.items():
+                        print("{}: {} opintopisteitä".format(subject, point))
+                        gsubjects.append("{}: {} opintopisteitä".format(subject, point))
+
+                    print("_______________________")
+                    print("You have a total of: ",summ(subnpoints),'"opintopisteitä"')
+                    total = font1.render(f"Sinulla on: {summ(subnpoints)} opintopisteitä",True,"DarkTurquoise")
+                    
+    screen.fill((95,130,162))
+    pygame.draw.rect(screen,"aliceblue",outputbackground)
+    pygame.draw.rect(screen,"aquamarine",opback)
+    credits = font2.render("OP calc v1, made by Rui", True, "Black")
+    screen.blit(credits,(10, height-30))
+
+    if active == True:
+        msg = ["welcome! / tervetuloa!",
+        "en:",
+        "remember to paste the wilma-data into op.txt, which is read automatically by this program.",
+        "(the op.txt file must be in the same folder as this program.)",
+        "fi:",
+        "muista liittää wilman kurssitarjoittimen datan op.txt:n sisälle, ohjelma lukee sen automaattisesti.",
+        "(op.txt tiedosto on oltava samassa kansiossa kuin tämä ohjelma."]
+        why = 20
+        for line in msg: #i hate pygame
+            message = pygame.font.SysFont('Arial',20).render(line,True,"Black")
+            screen.blit(message,(15,why))
+            why += 24
+
+        mouse = pygame.mouse.get_pos()
+    
+        button = pygame.draw.rect(screen,"white",calcb) 
+        pygame.draw.rect(screen,"orange",calcb, width=2)
+      
+        screen.blit(text, (calcb[0]+17,calcb[1]+4))
+
+        # updates the frames of the game
+        pygame.display.update()
+        clock.tick(60)
+
+    if active == False:
+        blt = font2.render('below, are your subjects and OP seperated by a ":"',True,"black")
+        blt2 = font2.render('alhaallasi on opinnot ja opintopisteet erotettu ":" lla',True,"black")
+        screen.blit(blt,(20,20))
+        screen.blit(blt2,(20,40))
+        y = 60
+        for subject in gsubjects:
+            screen.blit(font2.render(subject,True,"black"),(20,y))
+            y += 14
+
+        screen.blit(total,(20,opback[1]+10))
+        
+        pygame.display.update()
+        clock.tick(60)
+
+
